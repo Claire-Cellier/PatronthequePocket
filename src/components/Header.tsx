beae9b4dfbb43000c47d2406d1e801bd31a017c0
Claire Cellier <c.cellier@gmail.com>
2025-01-20 11:39:56 +0100

    corrections images relative path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header (){
     return (
         <header>
             <ul>
-            <li><img className="logo" src="src/images/logo-pp.jpg" alt="logo Polly Patronthèque"/></li>
+            <li><img className="logo" src="../images/logo-pp.jpg" alt="logo Polly Patronthèque"/></li>
             <li><h1>Patronthèque in the Pocket</h1></li>
             </ul>
         </header>
